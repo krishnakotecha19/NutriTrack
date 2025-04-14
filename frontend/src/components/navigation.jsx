6c026e77415d02daec9f23c4217c7d0a2b707b11
krishnakotecha19 <kotechakrishnaa@gmail.com>
2025-04-14 14:22:34 +0530

    added navigation.jsx

--- frontend/src/components/navigation.jsx
+++ frontend/src/components/navigation.jsx
@@ -6,18 +6,29 @@ const Navigation = () => {
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
 
   useEffect(() => {
-    // Check if userId exists in localStorage
     setIsUserLoggedIn(localStorage.getItem("user_id") !== null);
   }, []);
 
-  if (!isUserLoggedIn) return null; // Hide navbar if user is not logged in
+  if (!isUserLoggedIn) return null;
 
   return (
     <div className="navigation">
-      <Link to="/home" className="nav-item">Home</Link>
-      <Link to="/caloriein" className="nav-item">Calorie In</Link>
-      <Link to="/trends" className="nav-item">Trends</Link>
-      <Link to="/profile" className="nav-item">Profile</Link>
+      <Link to="/home" className="nav-item">
+        <img src={`${process.env.PUBLIC_URL}/home.png`} alt="Home" className="nav-icon" />
+       
+      </Link>
+      <Link to="/caloriein" className="nav-item">
+        <img src={`${process.env.PUBLIC_URL}/feast.png`} alt="Calorie In" className="nav-icon" />
+     
+      </Link>
+      <Link to="/trends" className="nav-item">
+        <img src={`${process.env.PUBLIC_URL}/trends.png`} alt="Trends" className="nav-icon" />
+       
+      </Link>
+      <Link to="/profile" className="nav-item">
+        <img src={`${process.env.PUBLIC_URL}/profile.png`} alt="Profile" className="nav-icon" />
+       
+      </Link>
     </div>
   );
 };
