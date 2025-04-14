6045196f8efe78ea70fed13fc5afb36a0cbe0aa2
krishnakotecha19 <kotechakrishnaa@gmail.com>
2025-04-14 13:41:26 +0530

    Updated logo name in WelcomePage

--- frontend/src/components/WelcomePage.jsx
+++ frontend/src/components/WelcomePage.jsx
@@ -8,7 +8,7 @@ function WelcomePage() {
   return (
     <div className="welcome-page">
       <div className="welcome-card">
-        <img src="/calories.png" alt="BalanceBites Logo" className="logo" />
+        <img src="/favicon.png" alt="BalanceBites Logo" className="logo" />
         <h1>Welcome to Feastify!</h1>
         <p>Lets manage your calories!!</p>
         <button onClick={() => navigate('/login')}>Let’s get started</button>
